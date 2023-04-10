7889cbdab2f6f7e393b73466a2206fafffc5cf8b
Lujan Sanchez Cracco <lsanchez@visual-engin.com>
2023-04-10 13:19:33 +0200

    Added error boundary to formattedDuration

---
+++ src/components/episodeCard/episodeCard_helpers.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import { Duration } from 'luxon';
+
+/**
+* Returns string with duration
+* @param {String} duration
+* @returns {String}
+*/
+const getFormattedDuration = duration => {
+	try {
+		if (duration.includes(':')) {
+			return duration;
+		} else {
+			const millisDuration = duration * 1000;
+			const objectDuration = Duration.fromMillis(millisDuration).shiftTo('minutes', 'seconds').toObject();
+			return `${objectDuration.minutes}:${objectDuration.seconds}`;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export { getFormattedDuration };
