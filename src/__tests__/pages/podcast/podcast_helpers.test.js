17ed0b3ed45ec0d7aa6aaf165225ce366c75481a
Lujan Sanchez Cracco <malh@gft.com>
2023-04-30 15:19:06 +0200

    Added podcast helpers tests

--- src/__tests__/pages/podcast/podcast_helpers.test.js
+++ src/__tests__/pages/podcast/podcast_helpers.test.js
@@ -1,3 +1,38 @@
 import axios from 'axios';
+import { getEpisodesByPodcastId } from '../../../pages/podcast/podcast_helpers';
 
 jest.mock('axios');
+
+
+describe('Test getEpisodesByPodcastId', () => {
+    const episodes = { data: { results:  [
+        { superfluosInfo: 'does not matter' },
+        { id: '123456' },
+        { id: '123123' }, 
+        { id: '34544' }
+    ]}}
+    const id = '99999'
+    const localPodcastInfo = { id: '23423423'};
+	const dispatchIsLoading = jest.fn();
+	const setEpisodeList = jest.fn();
+	const saveDataByIdToLocalStorage = jest.fn();
+
+	it('should call setEpisodeList, dispatchIsLoading and saveDataByIdToLocalStorage when correct arguments are passed and backend call response is 200', async () => {
+		axios.get.mockResolvedValue(episodes);
+		await getEpisodesByPodcastId(id, localPodcastInfo, setEpisodeList, dispatchIsLoading, saveDataByIdToLocalStorage);
+        const newEpisodes = [...episodes.data.results];
+
+		expect(dispatchIsLoading).toBeCalledTimes(2);
+		expect(setEpisodeList).toHaveBeenCalledWith(newEpisodes);
+		expect(saveDataByIdToLocalStorage).toHaveBeenCalledWith({...localPodcastInfo, episodes: newEpisodes}, id);
+	});
+
+	it('should call dispatchIsLoading, and NOT call setEpisodeList and saveDataByIdToLocalStorage when correct arguments are passed and backend call response is rejected', async () => {
+		axios.get.mockRejectedValue();
+		await getEpisodesByPodcastId(id, localPodcastInfo, setEpisodeList, dispatchIsLoading, saveDataByIdToLocalStorage);
+
+		expect(dispatchIsLoading).toBeCalledTimes(2);
+		expect(setEpisodeList).not.toHaveBeenCalled();
+		expect(saveDataByIdToLocalStorage).not.toHaveBeenCalled();
+	});
+});
